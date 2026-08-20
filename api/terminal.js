efbc7047d7fec40ffad8936c4b79cebc5ec8bc34
DarquetAlexis <alexx.malo.sss@gmail.com>
2026-08-20 13:19:01 -0600

    Update terminal.js

--- api/terminal.js
+++ api/terminal.js
@@ -1,14 +1,14 @@
 export default async function handler(req, res) {
-  // Permitir peticiones desde tu app web
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    // Permitir CORS
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
 
   const ACCESS_TOKEN = process.env.MP_ACCESS_TOKEN;
   try {
